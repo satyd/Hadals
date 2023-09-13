69237b7b3aa535f6d09b009e450f623c4965edd8
Leu Pashkouski <satyd@mail.ru>
2023-09-13 19:39:50 +0300

    added app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,11 +39,18 @@ android {
 }
 
 dependencies {
+    val nav_version = "2.7.1"
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    // Navigation
+    implementation("androidx.navigation:navigation-fragment-ktx:$nav_version")
+    implementation("androidx.navigation:navigation-ui-ktx:$nav_version")
+    implementation("com.github.bumptech.glide:glide:4.14.2")
+
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
